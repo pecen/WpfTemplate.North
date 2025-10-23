3c88138e6ac63a3a44fa3b82bb4bd739c8daadd9
PETERCENTEL5929\peter <peter.centellini@magello.se>
2025-10-23 14:35:43 +0200

    Added the UI.Common project

---
+++ src/UI/WpfTemplate.North.UI.Common/MVVM/Prism/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace WpfTemplate.North.UI.Common.MVVM.Prism
+{
+	public class ViewModelBase : BindableBase
+	{
+		public DelegateCommand<object>? NavigateCommand { get; set; }
+
+		private string? _title;
+		public string? Title
+		{
+			get => _title;
+			set => SetProperty(ref _title, value);
+		}
+	}
+}
--- src/UI/WpfTemplate.North.UI.Shell/MVVM/ViewModels/MainWindowViewModel.cs
+++ src/UI/WpfTemplate.North.UI.Shell/MVVM/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfTemplate.North.UI.Common.MVVM.Prism;
 
 namespace WpfTemplate.North.UI.Shell.MVVM.ViewModels
 {
-	public class MainWindowViewModel
+	public class MainWindowViewModel : ViewModelBase
 	{
 	}
 }
