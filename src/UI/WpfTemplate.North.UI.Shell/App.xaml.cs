d5cd529314a41d0191c8296ad724c5ecd0a8d494
PETERCENTEL5929\peter <peter.centellini@magello.se>
2025-10-23 13:50:49 +0200

    Added MahApps.Metro

--- src/UI/WpfTemplate.North.UI.Shell/App.xaml.cs
+++ src/UI/WpfTemplate.North.UI.Shell/App.xaml.cs
@@ -1,14 +1,29 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
 using System.Windows;
+using WpfTemplate.North.UI.Shell.Views;
 
 namespace WpfTemplate.North.UI.Shell
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application
+	public partial class App : PrismApplication
 	{
-	}
+		protected override Window CreateShell()
+		{
+			return Container.Resolve<MainWindow>();
+		}
 
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+
+		}
+
+		protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+		{
+			base.ConfigureModuleCatalog(moduleCatalog);
+		}
+	}
 }
