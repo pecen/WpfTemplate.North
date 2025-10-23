d891c632a4b865e440d19bd3659e6e845bd7566b
PETERCENTEL5929\peter <peter.centellini@magello.se>
2025-10-23 13:28:47 +0200

    Added the Shell project

---
+++ src/UI/WpfTemplate.North.UI.Shell/MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTemplate.North.UI.Shell.MVVM.ViewModels
+{
+	public class MainWindowViewModel
+	{
+	}
+}
---
+++ src/UI/WpfTemplate.North.UI.Shell/MVVM/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfTemplate.North.UI.Shell.Views
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
